d4954fe5ba8d3ae87882fcb5c050410da24927d1
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:47 +0800

    Remove POUR_REPOSITORY env var
    It doesn't seem to serve any useful purpose.

--- internal/cmd/shellenv.go
+++ internal/cmd/shellenv.go
@@ -14,14 +14,12 @@ func init() {
 	shellenvTemplates = make(map[string]*t.Template)
 	shellenvTemplates["sh"] = t.Must(t.New("sh").Funcs(funcMap).Parse(`POUR_PREFIX="{{.}}"; export POUR_PREFIX;
 POUR_CELLAR="{{.}}/Cellar"; export POUR_CELLAR;
-POUR_REPOSITORY="{{.}}/Lite"; export POUR_REPOSITORY;
 PATH="{{.}}/bin:{{.}}/sbin${PATH+:$PATH}"; export PATH;
 MANPATH="{{.}}/share/man${MANPATH+:$MANPATH}:"; export MANPATH;
 INFOPATH="{{.}}/share/info${INFOPATH+:$INFOPATH}"; export INFOPATH;
 `))
 	shellenvTemplates["bash"] = t.Must(t.New("bash").Funcs(funcMap).Parse(`export POUR_PREFIX="{{.}}";
 export POUR_CELLAR="{{.}}/Cellar";
-export POUR_REPOSITORY="{{.}}/Lite";
 export PATH="{{.}}/bin:{{.}}/sbin${PATH+:$PATH}";
 export MANPATH="{{.}}/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="{{.}}/share/info${INFOPATH+:$INFOPATH}";
@@ -29,7 +27,6 @@ export INFOPATH="{{.}}/share/info${INFOPATH+:$INFOPATH}";
 	shellenvTemplates["zsh"] = shellenvTemplates["bash"]
 	shellenvTemplates["fish"] = t.Must(t.New("fish").Funcs(funcMap).Parse(`set -gx POUR_PREFIX "{{.}}";
 set -gx POUR_CELLAR "{{.}}/Cellar";
-set -gx POUR_REPOSITORY "{{.}}/Homebrew";
 set -g fish_user_paths "{{.}}/bin" "{{.}}/sbin" $fish_user_paths;
 set -q MANPATH; or set MANPATH ''; set -gx MANPATH "{{.}}/share/man" $MANPATH;
 set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "{{.}}/share/info" $INFOPATH;
