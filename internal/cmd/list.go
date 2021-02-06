063e52da3fc3e290b7b156fea99bf07459ec5147
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:43 +0800

    Implement "list" command

--- internal/cmd/list.go
+++ internal/cmd/list.go
@@ -3,8 +3,37 @@ package cmd
 import (
 	"fmt"
 	"github.com/gromgit/litebrew/internal/formula"
+	"os"
+	"path/filepath"
 )
 
 func List(formulas formula.Formulas, args []string) {
-	fmt.Println("This command has not been implemented yet.")
+	if len(args) > 0 {
+		for _, name := range args {
+			f := formulas[name]
+			if f.Name != "" {
+				err := filepath.Walk(f.InstallDir,
+					func(path string, info os.FileInfo, err error) error {
+						if err != nil {
+							return err
+						}
+						if info.IsDir() {
+							// Skip .brew dir
+							if info.Name() == ".brew" {
+								return filepath.SkipDir
+							}
+						} else {
+							fmt.Println(path)
+						}
+						return nil
+					})
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	} else {
+		// List installed bottles
+		formulas.Filter(func(item formula.Formula) bool { return item.Status != formula.MISSING }).Ls()
+	}
 }
