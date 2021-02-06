482f272c941a1a4c9fed7ec073c9910140073c39
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:49 +0800

    Revamped install process
    * Make list of all wanted bottles + deps before actual install
    * Treat upgrades as equivalent to installs
    * Respect pinning

--- internal/cmd/install.go
+++ internal/cmd/install.go
@@ -3,28 +3,71 @@ package cmd
 import (
 	"fmt"
 	"github.com/gromgit/pour/internal/bottle"
-	"github.com/gromgit/pour/internal/config"
+	cfg "github.com/gromgit/pour/internal/config"
 	"github.com/gromgit/pour/internal/formula"
 	"path/filepath"
+	"strings"
 )
 
+const (
+	NOTHING = iota
+	UPGRADE
+	INSTALL
+	// Leaf bit
+	LEAF     = 1 << iota
+	ACT_MASK = LEAF - 1
+)
+
+type InstallMap map[string]int
+
+func resolveDependencies(allf *formula.Formulas, name string, depMap InstallMap, leaf bool) error {
+	// Only "local" bottles need apply
+	if strings.Contains(name, "/") {
+		return fmt.Errorf("Unable to install foreign bottle '%s'", name)
+	}
+	f := (*allf)[name]
+	if f.Name == "" {
+		return fmt.Errorf("No such bottle '%s'", name)
+	}
+	s := NOTHING
+	switch f.Status {
+	case formula.MISSING:
+		s = INSTALL
+	case formula.OUTDATED:
+		if f.Pinned {
+			return fmt.Errorf("Can't update pinned bottle '%s'", name)
+		} else {
+			s = UPGRADE
+		}
+	}
+	if leaf {
+		s |= LEAF
+	}
+	if depMap[name] == NOTHING {
+		depMap[name] = s
+	}
+	for _, dep := range f.Dependencies {
+		if err := resolveDependencies(allf, dep, depMap, false); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func Install(allf *formula.Formulas, args []string) (err error) {
+	instMap := make(InstallMap)
 	for _, name := range args {
+		if err = resolveDependencies(allf, name, instMap, true); err != nil {
+			return
+		}
+	}
+	// Do all the installations
+	cfg.Logf("instMap: %+v\n", instMap)
+	for name, act := range instMap {
 		f := (*allf)[name]
-		if f.Name != "" {
-			// First install all dependencies
-			Install(allf, f.Dependencies)
-			switch f.Status {
-			case formula.INSTALLED:
-				fmt.Println("DEBUG: Already installed", f.Name)
-				continue
-			case formula.OUTDATED:
-				fmt.Println("DEBUG: Upgrade", f.Name)
-			case formula.MISSING:
-				fmt.Println("DEBUG: Install", f.Name)
-				if err = bottle.Install(f); err != nil {
-					return
-				}
+		if act&ACT_MASK != NOTHING {
+			if err = bottle.Install(f, (act&LEAF > 0)); err != nil {
+				return
 			}
 		}
 	}
@@ -35,7 +78,7 @@ func Link(allf *formula.Formulas, args []string) (err error) {
 	for _, name := range args {
 		f := (*allf)[name]
 		if f.Status != formula.MISSING && f.InstallDir != "" {
-			rel, err := filepath.Rel(config.CELLAR, f.InstallDir)
+			rel, err := filepath.Rel(cfg.CELLAR, f.InstallDir)
 			if err != nil {
 				return err
 			}
