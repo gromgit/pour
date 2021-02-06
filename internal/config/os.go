4413f77441fffdeb704a1a9ca6b28f1925ad67b4
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:44 +0800

    Use our own /var hierarchy
    No sense messing with Homebrew's.

--- internal/config/os.go
+++ internal/config/os.go
@@ -5,9 +5,9 @@ import oscfg "github.com/gromgit/litebrew/internal/config/os"
 
 const DEFAULT_PREFIX = oscfg.DEFAULT_PREFIX
 const JSON_URL = oscfg.JSON_URL
-const JSON_PATH = DEFAULT_PREFIX + "/var/homebrew/bottles.json"
+const JSON_PATH = DEFAULT_PREFIX + "/var/litebrew/bottles.json"
 const CELLAR = DEFAULT_PREFIX + "/Cellar"
-const PINDIR = DEFAULT_PREFIX + "/var/homebrew/pinned"
-const LINKDIR = DEFAULT_PREFIX + "/var/homebrew/linked"
+const PINDIR = DEFAULT_PREFIX + "/var/litebrew/pinned"
+const LINKDIR = DEFAULT_PREFIX + "/var/litebrew/linked"
 
 var OS_FIELD = oscfg.GetOS()
