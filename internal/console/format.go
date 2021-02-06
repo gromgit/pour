901350d81a7199d912a8b8c85a6dd3630aaf9514
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:41 +0800

    Revert to simple COLUMNS-based sizing
    If folks want proper operation, they can "export COLUMNS".

--- internal/console/format.go
+++ internal/console/format.go
@@ -2,9 +2,9 @@ package console
 
 import (
 	"fmt"
-	"github.com/gromgit/goncurses"
 	"github.com/mattn/go-isatty"
 	"os"
+	"strconv"
 	"unicode/utf8"
 )
 
@@ -26,55 +26,49 @@ func (s FancyString) Print(strfmt string) string {
 
 func Columnate(strs []FancyString) string {
 	nstrs := len(strs)
-	if nstrs == 0 {
-		goto nothing
-	}
-	if !isatty.IsTerminal(os.Stdin.Fd()) {
-		// Not a terminal, do single column
-		return PlainColumn(strs)
-	}
-	if stdscr, err := goncurses.Init(); err != nil {
-		panic(err)
-	} else {
-		defer goncurses.End()
-		// On a terminal, so let's get fancy
-		screenheight, screenwidth := stdscr.MaxYX()
-		if screenwidth == 0 {
-			panic(fmt.Sprintf("ARGH! MaxYX = (%d,%d)", screenheight, screenwidth))
-		}
-		// First find the longest width
-		maxwidth := 0
-		for _, s := range strs {
-			l := utf8.RuneCountInString(s.Content)
-			if l > maxwidth {
-				maxwidth = l + 2 // Have at least two spaces for each column
+	result := ""
+	if nstrs > 0 {
+		if isatty.IsTerminal(os.Stdin.Fd()) {
+			// Now find the screen width
+			screenwidth, err := strconv.Atoi(os.Getenv("COLUMNS"))
+			if err != nil || screenwidth == 0 {
+				screenwidth = 80 // A decent default
 			}
-		}
-		strfmt := fmt.Sprintf("%%-%ds", maxwidth)
-		cols := screenwidth / maxwidth
-		stride := nstrs / cols
-		fmt.Fprintf(os.Stderr, "Fmt: %q  Screen: (%d,%d)  Cols: %d  Stride: %d\n", strfmt, screenheight, screenwidth, cols, stride)
-		// Let's run through the elements
-		result := ""
-		if stride == 0 {
-			// Single row, so just walk through strs
+			// First find the longest width
+			maxwidth := 0
 			for _, s := range strs {
-				result = result + fmt.Sprintf(strfmt, s.Print(strfmt))
+				l := utf8.RuneCountInString(s.Content)
+				if l > maxwidth {
+					maxwidth = l
+				}
 			}
-			result = result + "\n"
-		} else {
-			// Striding through the stringies...
-			for y := 0; y < stride; y++ {
-				for x := 0; x < cols; x++ {
-					if x*stride+y < nstrs {
-						result = result + fmt.Sprintf(strfmt, strs[x*stride+y].Print(strfmt))
-					}
+			maxwidth += 2 // Add two spaces to each column
+			strfmt := fmt.Sprintf("%%-%ds", maxwidth)
+			cols := screenwidth / maxwidth
+			stride := nstrs / cols
+			fmt.Fprintf(os.Stderr, "Fmt: %q  Screen: (%d,%d)  Cols: %d  Stride: %d\n", strfmt, 0, screenwidth, cols, stride)
+			// Let's run through the elements
+			if stride == 0 {
+				// Single row, so just walk through strs
+				for _, s := range strs {
+					result = result + fmt.Sprintf(strfmt, s.Print(strfmt))
 				}
 				result = result + "\n"
+			} else {
+				// Striding through the stringies...
+				for y := 0; y < stride; y++ {
+					for x := 0; x < cols; x++ {
+						if x*stride+y < nstrs {
+							result = result + fmt.Sprintf(strfmt, strs[x*stride+y].Print(strfmt))
+						}
+					}
+					result = result + "\n"
+				}
 			}
+		} else {
+			// Not a terminal, do single column
+			result = PlainColumn(strs)
 		}
-		return result
 	}
-nothing:
-	return "" // Nothing to do
+	return result
 }
