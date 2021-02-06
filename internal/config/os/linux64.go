376c32dc097440a2dd770dd968c42d99d39df957
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:43 +0800

    [FIX] Linux config is 64-bit only

--- internal/config/os/linux64.go
+++ internal/config/os/linux64.go
@@ -6,6 +6,6 @@ const JSON_URL = "https://formulae.brew.sh/api/formula-linux.json"
 const DEFAULT_PREFIX = "/home/linuxbrew/.linuxbrew"
 
 func GetOS() (os string) {
-os = "Linux64"
-return
+	os = "Linux64"
+	return
 }
