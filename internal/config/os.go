cadf2f06ad4233aa30557afee56aeac7479d7842
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:46 +0800

    Add basic central logger + some other dirs

--- internal/config/os.go
+++ internal/config/os.go
@@ -1,7 +1,11 @@
 package config
 
 // Import the OS-specific stuff
-import oscfg "github.com/gromgit/litebrew/internal/config/os"
+import (
+	oscfg "github.com/gromgit/litebrew/internal/config/os"
+	"log"
+	"os"
+)
 
 const OS_NAME = oscfg.OS_NAME
 const DEFAULT_PREFIX = oscfg.DEFAULT_PREFIX
@@ -11,5 +15,14 @@ const VAR_PATH = DEFAULT_PREFIX + "/var/litebrew"
 const JSON_PATH = VAR_PATH + "/bottles.json"
 const PINDIR = VAR_PATH + "/pinned"
 const LINKDIR = VAR_PATH + "/linked"
+const BOTTLEDIR = VAR_PATH + "/bottles"
+const TEMPDIR = VAR_PATH + "/tmp"
 
 var OS_FIELD = oscfg.GetOS()
+
+// Private logger instance
+var logger = log.New(os.Stderr, "litebrew", log.LstdFlags)
+
+func Log(v ...interface{}) {
+	logger.Println(v)
+}
