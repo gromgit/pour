9e5d7ed0772e9ad0ec719201092e560d038f4414
Adrian Ho <the.gromgit@gmail.com>
2021-02-06 21:54:42 +0800

    Implement "info" cmd

--- internal/cmd/info.go
+++ internal/cmd/info.go
@@ -1,10 +1,40 @@
 package cmd
 
 import (
-	"fmt"
+	"github.com/gromgit/litebrew/internal/console"
 	"github.com/gromgit/litebrew/internal/formula"
+	"log"
+	"os"
+	t "text/template"
 )
 
+type templateMap map[string]*t.Template
+
+var templates templateMap
+
+var funcMap = t.FuncMap{
+	"status": func(status int) string { return formula.StatusMap[status] },
+	"url":    func(url string) string { return console.Underscore + url + console.Off },
+}
+
+func init() {
+	templates = make(templateMap)
+	templates["main"] = t.Must(t.New("main").Funcs(funcMap).Parse(`{{.Name}}: stable {{.GetVersion}}{{if .Pinned}} [pinned]{{end}}
+{{.Desc}}
+{{url .Homepage}}
+{{.InstallDir}}{{status .Status}}
+From: {{url .Bottle.Stable.URL}}
+`))
+}
+
 func Info(formulas formula.Formulas, args []string) {
-	fmt.Println("This command has not been implemented yet.")
+	t := templates["main"]
+	for _, i := range args {
+		if f := formulas[i]; f.Name != "" {
+			err := t.Execute(os.Stdout, f)
+			if err != nil {
+				log.Println("executing template:", err)
+			}
+		}
+	}
 }
